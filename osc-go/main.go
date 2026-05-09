979fcbaee4f270cef6df9ace3556e8d7dc6e6aba
Hadrian <h@hadrian.cc>
2026-05-09 10:54:06 +0800

    feat: go lang

--- osc-go/main.go
+++ osc-go/main.go
@@ -2,17 +2,60 @@ package main
 
 import (
 	"fmt"
-	"osc-go/hockeyCalculations/calculateFlex"
-	"osc-go/hockeyCalculations/calculateLength"
-	// "math"
+	"math"
 )
 
+func inputArray(numberOfItems int, section string) []float64{
+	fmt.Printf("Enter your %v: ", section)
+	var ans=make([]float64, numberOfItems)
+	for i:=0; i<numberOfItems; i++{
+		fmt.Scan(&ans[i])
+	}
+	return ans
+}
+
+func getGradeLevel(mark int) string{
+	var gradeLevel string
+	switch {
+		case mark>=80:
+			gradeLevel="4"
+		case mark>=70:
+			gradeLevel="3"
+		case mark>=60:
+			gradeLevel="2"
+		case mark>=50:
+			gradeLevel="1"
+		default:
+			gradeLevel="R"
+	}
+	return gradeLevel
+}
+
 func main() {
-	var weight int=47;
-	var height int=162;
-	var position string="forward"
-	flex:=calculateFlex.Calculate(weight)
-	length:=calculateLength.Calculate(height, position)
-	fmt.Println("Your ideal flex should be", flex, "flex.")
-	fmt.Printf("Your stick length should be %v\".\n", length)
+	var numberOfItems int;
+	fmt.Printf("Enter the number of items: ")
+	fmt.Scanln(&numberOfItems)
+
+	var marks=inputArray(numberOfItems, "marks")
+	var denominators=inputArray(numberOfItems, "denominators")
+	var weights=inputArray(numberOfItems, "weights")
+
+	var totalWeight float64
+	for _, v:=range weights{
+		totalWeight+=v
+	}
+
+	var exactMark float64
+	for i:=0; i<numberOfItems; i++{
+		var mark=marks[i]/denominators[i]
+		var weight=weights[i]/totalWeight
+
+		exactMark += mark * weight
+	}
+
+	exactMark*=100
+	var finalMark int=int(math.Round(exactMark))
+	var gradeLevel=getGradeLevel(finalMark)
+
+	fmt.Printf("Your final mark is %v%%, which is a level %v.\n", finalMark, gradeLevel)
 }
